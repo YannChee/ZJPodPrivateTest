875a3d0a6326e93fee5d053f1dbc53624e85d233
YannChee <yannchee@163.com>
2019-02-13 15:17:15 +0800

    第一次提交

---
+++ ZJPodPrivateTest/Classes/NSString+QYTest.h
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+
+@interface NSString (QYTest)
+- (void)test;
+@end
---
+++ ZJPodPrivateTest/Classes/NSString+QYTest.m
@@ -0,0 +1,7 @@
+#import "NSString+QYTest.h"
+
+@implementation NSString (QYTest)
+- (void)test{
+    NSLog(@"只是一个测试");
+}
+@end
